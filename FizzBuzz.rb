250c7030a96466f02fa6b0277871078687c1a262
hatimh <hatim@ualberta.ca>
2016-01-02 12:28:48 -0700

    Update FizzBuzz.rb

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,10 +1,10 @@
-String = ""
+aString = ""
 for number in 1..100
 
-String += "Fizz" if number % 3 == 0
-String += "Buzz" if number % 5 == 0
-String = number if String.length==0
+aString += "Fizz" if number % 3 == 0
+aString += "Buzz" if number % 5 == 0
+aString = number if String.length==0
 
-puts String
-String = ""
+puts aString
+aString = ""
 end
